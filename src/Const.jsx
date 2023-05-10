d7a1298565dd3a362471690fc7976249d4f0c76c
Muhammed-Rashid-07 <rashid24601@gmail.com>
2023-05-10 23:07:10 +0530

    Added sheet as database download pdf feature

--- src/Const.jsx
+++ src/Const.jsx
@@ -1,23 +1,25 @@
 import * as yup from "yup";
+import axios from "axios";
 
-export const initialValues  = {
-  name: "",
-  mobileNumber: "",
-  whatsappNumber: "",
-  singleWindowNo: "",
-  examName: "",
-  regNumber: "",
-  year: "",
-  schoolName: "",
-  gender: "",
-  religion: "",
-  dateOfBirth: "",
-  motherName: "",
-  fatherName: "",
-  houseName: "",
-  postOffice: "",
-  panchayath: "",
-  ward: "",
+
+export const initialValues = {
+  Name: "",
+  MobileNumber: "",
+  WhatsappNumber: "",
+  SingleWindowNo: "",
+  ExamName: "",
+  RegNumber: "",
+  Year: "",
+  SchoolName: "",
+  Gender: "",
+  Religion: "",
+  DateOfBirth: "",
+  MotherName: "",
+  FatherName: "",
+  HouseName: "",
+  PostOffice: "",
+  Panchayath: "",
+  Ward: "",
   subjectsMarks: [
     { subject: 'Lang1', grade: '' },
     { subject: 'Lang2', grade: '' },
@@ -35,37 +37,37 @@ export const initialValues  = {
 };
 
 export const validationSchema = yup.object().shape({
-  name: yup.string().required("Name of Applicant is required"),
-  mobileNumber: yup
+  Name: yup.string().required("Name of Applicant is required"),
+  MobileNumber: yup
     .string()
     .matches(/^\d+$/, "Mobile number must be a valid number")
     .min(10, "Mobile number must be at least 10 digits")
     .required("Mobile number is required"),
-  whatsappNumber: yup
+  WhatsappNumber: yup
     .string()
     .matches(/^\d+$/, "Whatsapp number must be a valid number")
     .min(10, "Whatsapp number must be at least 10 digits")
     .required("Whatsapp number is required"),
-  singleWindowNo: yup
+  SingleWindowNo: yup
     .string()
     .matches(/^\d+$/, "Single Window number must be a valid number")
     .required("Single Window number is required"),
-  examName: yup.string().required("Name of qualifying examination is required"),
-  regNumber: yup
+  ExamName: yup.string().required("Name of qualifying examination is required"),
+  RegNumber: yup
     .string()
     .matches(/^\d+$/, "Single Window number must be a valid number")
     .required("Register Number is required"),
-  year: yup.string().required("Year is required"),
-  schoolName: yup.string().required("Name of school is required"),
-  gender: yup.string().required("Gender is required"),
-  religion: yup.string().required("Religion is required"),
-  dateOfBirth: yup.string().required("Date of Birth is required"),
-  motherName: yup.string().required("Name of Mother is required"),
-  fatherName: yup.string().required("Name of Father is required"),
-  houseName: yup.string().required("House Name is required"),
-  postOffice: yup.string().required("Post Office is required"),
-  panchayath: yup.string().required("Panchayath is required"),
-  ward: yup.string().required("Ward is required"),
+  Year: yup.string().required("Year is required"),
+  SchoolName: yup.string().required("Name of school is required"),
+  Gender: yup.string().required("Gender is required"),
+  Religion: yup.string().required("Religion is required"),
+  DateOfBirth: yup.string().required("Date of Birth is required"),
+  MotherName: yup.string().required("Name of Mother is required"),
+  FatherName: yup.string().required("Name of Father is required"),
+  HouseName: yup.string().required("House Name is required"),
+  PostOffice: yup.string().required("Post Office is required"),
+  Panchayath: yup.string().required("Panchayath is required"),
+  Ward: yup.string().required("Ward is required"),
   subjectsMarks: yup.array().of(
     yup.object().shape({
       subject: yup.string().required('Required'),
@@ -74,11 +76,11 @@ export const validationSchema = yup.object().shape({
   ),
   coursePreference1: yup.string().required('Course preference 1 is required'),
 });
- 
- export const years = [];
- for (let i = new Date().getFullYear(); i > 1900; i--) {
-   years.push({value:i, label:i});
- }
+
+export const years = [];
+for (let i = new Date().getFullYear(); i > 1900; i--) {
+  years.push({ value: i, label: i });
+}
 
 export const genderOptions = [
   { value: 'male', label: 'Male' },
@@ -113,7 +115,7 @@ export const panchayatOptions = [
 ]
 
 export const subjects = [
-  'Lang1','Lang2','Eng','Hindi','SS','Phy','Chem','Bio','Maths','IT'
+  'Lang1', 'Lang2', 'Eng', 'Hindi', 'SS', 'Phy', 'Chem', 'Bio', 'Maths', 'IT'
 ]
 
 export const gradesOptions = [
@@ -136,3 +138,45 @@ export const courseOptions = [
 ]
 
 
+
+export const handleSubmit = async (values) => {
+  try {
+    const formattedValues = {
+      Name: values.Name,
+      MobileNumber: values.MobileNumber,
+      WhatsappNumber: values.WhatsappNumber,
+      SingleWindowNo: values.SingleWindowNo,
+      ExamName: values.ExamName,
+      RegNumber: values.RegNumber,
+      Year: values.Year,
+      SchoolName: values.SchoolName,
+      Gender: values.Gender,
+      Religion: values.Religion,
+      DateOfBirth: values.DateOfBirth,
+      MotherName: values.MotherName,
+      FatherName: values.FatherName,
+      HouseName: values.HouseName,
+      PostOffice: values.PostOffice,
+      Panchayath: values.Panchayath,
+      Ward: values.Ward,
+      Lang1: values.subjectsMarks[0].grade,
+      Lang2: values.subjectsMarks[1].grade,
+      Eng: values.subjectsMarks[2].grade,
+      Hindi: values.subjectsMarks[3].grade,
+      SS: values.subjectsMarks[4].grade,
+      Phy: values.subjectsMarks[5].grade,
+      Chem: values.subjectsMarks[6].grade,
+      Bio: values.subjectsMarks[7].grade,
+      Maths: values.subjectsMarks[8].grade,
+      IT: values.subjectsMarks[9].grade,
+      coursePreference1: values.coursePreference1,
+      coursePreference2: values.coursePreference2,
+    };
+    //Google sheet heading format
+    axios.post("https://sheet.best/api/sheets/2112eec1-f365-43b1-b918-287af866f358", formattedValues).then(response => {
+      console.log("response", response);
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,6 +19,9 @@ import {
 import { SelectInput } from "../components/common/SelectInput";
 import SubjectsGradeInput from "../components/SubjectsGradeInput";
 import CourseInput from "../components/CourseInput";
+import { handleSubmit, subjects } from '../Const'
+import { Page, Text, Document, StyleSheet, View, PDFDownloadLink } from "@react-pdf/renderer";
+import { useState } from "react";
 
 const FormField = styled(Field)`
   & input::-webkit-outer-spin-button,
@@ -43,13 +46,149 @@ const FormContainer = styled(Form)`
 `;
 
 function RegistrationForm() {
+
+  const [formValues, setFormValues] = useState(initialValues)
+  const styles = StyleSheet.create({
+    page: {
+      display: 'flex',
+      backgroundColor: '#E4E4E4',
+      padding: '20px',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    section: {
+      margin: '10px',
+      padding: '10px',
+      flexGrow: 1,
+      backgroundColor: '#FFFFFF',
+      borderRadius: '10px',
+      boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+    },
+
+    header: {
+      fontSize: '24px',
+      marginBottom: '20px',
+      color: '#333333',
+      textAlign: 'center',
+      fontWeight: 'bold',
+    },
+
+    field: {
+      marginBottom: '10px',
+      fontSize: '14px',
+      textAlign: 'left',
+      marginLeft: '20px',
+    },
+
+    tableContainer: {
+      marginTop: '10px',
+      marginBottom: '20px',
+      marginLeft: '20px',
+    },
+    tableHeader: {
+      display: 'flex',
+      flexDirection: 'row',
+      backgroundColor: '#EEEEEE',
+      padding: '5px 0',
+      borderBottom: '1px solid #CCCCCC',
+    },
+    tableRow: {
+      display: 'flex',
+      flexDirection: 'row',
+      padding: '5px 0',
+      borderBottom: '1px solid #CCCCCC',
+    },
+    tableCell: {
+      flex: 1,
+      fontSize: '12px',
+      textAlign: 'left',
+      paddingLeft: '5px',
+    },
+
+  })
+
+  const MyDocument = () => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text style={styles.header}>Application for PlusOne Admission 2023-24 (Management Quota)</Text>
+          <View style={styles.field}>
+            <Text>Name: {formValues.Name}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Mobile Number: {formValues.MobileNumber}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>WhatsappNo: {formValues.WhatsappNumber}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>DateofBirth: {formValues.DateOfBirth}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Exam Name: {formValues.ExamName}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Register Number: {formValues.RegNumber}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Year: {formValues.Year}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>School Name: {formValues.SchoolName}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Father Name: {formValues.FatherName}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Mother Name: {formValues.MotherName}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>House Name: {formValues.HouseName}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>School Name: {formValues.PostOffice}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>School Name: {formValues.Panchayath}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>School Name: {formValues.Ward}</Text>
+          </View>
+          <View style={styles.tableContainer}>
+            <View style={styles.tableHeader}>
+              <Text style={styles.tableCell}>Subject</Text>
+              <Text style={styles.tableCell}>Grade</Text>
+            </View>
+            {formValues.subjectsMarks.map((subject, index) => (
+              <View key={index} style={styles.tableRow}>
+                <Text style={styles.tableCell}>{subject.subject}</Text>
+                <Text style={styles.tableCell}>{subject.grade}</Text>
+              </View>
+            ))}
+          </View>
+          <View style={styles.field}>
+            <Text>Course Preference 1: {formValues.coursePreference1}</Text>
+          </View>
+          <View style={styles.field}>
+            <Text>Course Preference 2: {formValues.coursePreference2}</Text>
+          </View>
+
+        </View>
+      </Page>
+    </Document>
+  );
+
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
+      //on submit section
       onSubmit={(values) => {
+        handleSubmit(values)
+        setFormValues(values)
+        console.log(values);
         alert(JSON.stringify(values, null, 2));
-        console.table(values)
+
       }}
     >
       {({ errors, touched }) => (
@@ -69,18 +208,19 @@ function RegistrationForm() {
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="name"
+                name="Name"
                 label="Name of Applicant"
                 type="text"
                 fullWidth
                 error={errors.name && touched.name}
                 helperText={errors.name && touched.name && errors.name}
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="mobileNumber"
+                name="MobileNumber"
                 label="Mobile Number"
                 type="number"
                 fullWidth
@@ -90,12 +230,13 @@ function RegistrationForm() {
                   touched.mobileNumber &&
                   errors.mobileNumber
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="whatsappNumber"
+                name="WhatsappNumber"
                 label="Whatsapp Number"
                 type="number"
                 fullWidth
@@ -105,12 +246,13 @@ function RegistrationForm() {
                   touched.whatsappNumber &&
                   errors.whatsappNumber
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="singleWindowNo"
+                name="SingleWindowNo"
                 label="Single Window Appln. No."
                 type="number"
                 fullWidth
@@ -120,12 +262,13 @@ function RegistrationForm() {
                   touched.singleWindowNo &&
                   errors.singleWindowNo
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="examName"
+                name="ExamName"
                 label="Name of qualifying examination"
                 type="text"
                 fullWidth
@@ -133,12 +276,13 @@ function RegistrationForm() {
                 helperText={
                   errors.examName && touched.examName && errors.examName
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="regNumber"
+                name="RegNumber"
                 label="Register Number"
                 type="number"
                 fullWidth
@@ -146,11 +290,12 @@ function RegistrationForm() {
                 helperText={
                   errors.regNumber && touched.regNumber && errors.regNumber
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={3}>
               <SelectInput
-                name="year"
+                name="Year"
                 label="Year"
                 options={years}
                 error={errors.year && touched.year}
@@ -159,7 +304,7 @@ function RegistrationForm() {
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="schoolName"
+                name="SchoolName"
                 label="Name of school in which studied"
                 type="text"
                 fullWidth
@@ -167,11 +312,12 @@ function RegistrationForm() {
                 helperText={
                   errors.schoolName && touched.schoolName && errors.schoolName
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={6}>
               <SelectInput
-                name="gender"
+                name="Gender"
                 label="Gender"
                 options={genderOptions}
                 error={errors.gender && touched.gender}
@@ -179,7 +325,7 @@ function RegistrationForm() {
             </Grid>
             <Grid item xs={6}>
               <SelectInput
-                name="religion"
+                name="Religion"
                 label="Religion"
                 options={religionOptions}
                 error={errors.religion && touched.religion}
@@ -188,7 +334,7 @@ function RegistrationForm() {
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="dateOfBirth"
+                name="DateOfBirth"
                 label="Date of Birth"
                 type="date"
                 InputLabelProps={{
@@ -199,25 +345,13 @@ function RegistrationForm() {
                 helperText={
                   errors.dateOfBirth && touched.dateOfBirth && errors.dateOfBirth
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12}>
               <FormField
                 as={TextField}
-                name="motherName"
-                label="Name of Mother"
-                type="text"
-                fullWidth
-                error={errors.motherName && touched.motherName}
-                helperText={
-                  errors.motherName && touched.motherName && errors.motherName
-                }
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormField
-                as={TextField}
-                name="fatherName"
+                name="FatherName"
                 label="Name of Father"
                 type="text"
                 fullWidth
@@ -225,12 +359,27 @@ function RegistrationForm() {
                 helperText={
                   errors.fatherName && touched.fatherName && errors.fatherName
                 }
+                margin="dense"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormField
+                as={TextField}
+                name="MotherName"
+                label="Name of Mother"
+                type="text"
+                fullWidth
+                error={errors.motherName && touched.motherName}
+                helperText={
+                  errors.motherName && touched.motherName && errors.motherName
+                }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <FormField
                 as={TextField}
-                name="houseName"
+                name="HouseName"
                 label="House Name"
                 type="text"
                 fullWidth
@@ -238,12 +387,13 @@ function RegistrationForm() {
                 helperText={
                   errors.houseName && touched.houseName && errors.houseName
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <FormField
                 as={TextField}
-                name="postOffice"
+                name="PostOffice"
                 label="Post Office"
                 type="text"
                 fullWidth
@@ -251,34 +401,56 @@ function RegistrationForm() {
                 helperText={
                   errors.postOffice && touched.postOffice && errors.postOffice
                 }
+                margin="dense"
               />
             </Grid>
             <Grid item xs={6}>
               <SelectInput
-                name="panchayath"
+                name="Panchayath"
                 label="Panchayath"
                 options={panchayatOptions}
                 error={errors.panchayath && touched.panchayath}
+
               />
             </Grid>
             <Grid item xs={6}>
               <FormField
                 as={TextField}
-                name="ward"
+                name="Ward"
                 label="Ward"
                 type="number"
                 fullWidth
                 error={errors.ward && touched.ward}
                 helperText={errors.ward && touched.ward && errors.ward}
+
               />
             </Grid>
           </Grid>
           <Divider className="py-4" />
-          <SubjectsGradeInput/>
-          <CourseInput/>
+          <SubjectsGradeInput subjects={subjects} label="Grades" name="subjectsMarks" />
+          <CourseInput />
           <SubmitButton variant="contained" type="submit">
             Submit
           </SubmitButton>
+          <PDFDownloadLink document={<MyDocument formValues={formValues} />} fileName='form_data.pdf'>
+            {({ blob, url, loading, error }) => {
+              if (loading) {
+                return 'Loading document...';
+              } else if (error) {
+                return `Error: ${error.message}`;
+              } else if (url) {
+                return (
+                  <div>
+                    <Button component="a" href={url} download="form_data.pdf">
+                      Download PDF
+                    </Button>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            }}
+          </PDFDownloadLink>
         </FormContainer>
       )}
     </Formik>
