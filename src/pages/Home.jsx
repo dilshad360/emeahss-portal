b9f70cd8be29d151f5dfddc2f4c423ce1d2ef419
dilshad360 <dilshad360.dev@gmail.com>
2023-05-22 10:58:34 +0530

    update: icons & bg image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Paper, Box } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import backgroundImage from '../assets/schoolbg.jpg';
+import logo from '../assets/emea_logo.jpeg';
 
 function Home() {
   return (
-    <div className="flex justify-center items-center flex-col h-screen gap-4">
-      <img className="w-20" src="/EMEAHSSlogo.png" alt="logo"></img>
+    <div className="flex justify-center items-center h-screen" style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }}>
+     <Box>
+    <Paper  elevation={3} className="flex justify-center items-center flex-col gap-4 px-6 py-12">
+      <img className="w-20" src={logo} alt="logo"></img>
       <Typography
         variant="h4"
         style={{
           fontWeight: "bold",
+          color: "darkgreen"
         }}
       >
         EMEAHSS - Portal
@@ -20,6 +30,8 @@ function Home() {
       <Button variant="contained">
         <Link to="/application">Download Application</Link>
       </Button>
+      </Paper >
+    </Box>
     </div>
   );
 }
