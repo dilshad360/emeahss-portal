e34ca4de231f7f7d8d4d3e4f04a6b8dd11a0b59a
dilshad360 <dilshad360.dev@gmail.com>
2024-06-15 19:08:51 +0530

    temp closed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
       <Route path='*' element={<Home/>}/>
       <Route path='/' element={<Home/>} />
-      <Route path='/management-form' element={<ManagementForm/>} />
+      {/* <Route path='/management-form' element={<ManagementForm/>} /> */}
       <Route path='/management-application' element={<Application/>} />
 
 
